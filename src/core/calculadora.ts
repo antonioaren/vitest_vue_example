4e12d55d29f97a5b0e1ce5df822346a9b204e231
antonioaren <antonioaren2@gmail.com>
2024-03-02 10:42:44 +0100

    chore: Make firt functional calculator to add test

---
+++ src/core/calculadora.ts
@@ -0,0 +1,14 @@
+class Calculadora {
+    public sum(a: number, b: number): number {
+        return a + b;
+    }
+    public substract(a: number, b: number): number {
+        return a - b;
+    }
+    public multiply(a: number, b: number): number {
+        return a * b;
+    }
+    public divide(a: number, b: number): number {
+        return a / b;
+    }
+}
