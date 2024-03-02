fa0afb56a4223ae938e90ca44b007eabaf73b935
antonioaren <antonioaren2@gmail.com>
2024-03-02 11:14:39 +0100

    feat: Calculator working more less and looks nice, nice to start testing (Should be more TDD, but what I want it is to see if my test are ok)

---
+++ src/core/calculator.ts
@@ -0,0 +1,20 @@
+class Calculator {
+  public sum(a: number, b: number): number {
+    return a + b
+  }
+
+  public substract(a: number, b: number): number {
+    return a - b
+  }
+
+  public multiply(a: number, b: number): number {
+    return a * b
+  }
+
+  public divide(a: number, b: number): number {
+    return a / b
+  }
+}
+
+export default new Calculator()
+
