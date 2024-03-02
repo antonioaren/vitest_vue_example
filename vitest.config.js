f481fba1a96562ac9b5fada4f1580204ba2c7d7b
Pedro Arenas <antonioaren2@gmail.com>
2024-03-02 16:59:52 +0100

    chore: improving configuration

--- vitest.config.js
+++ vitest.config.js
@@ -7,7 +7,13 @@ export default mergeConfig(
     defineConfig({
         test: {
             environment: 'jsdom',
-            exclude: [...configDefaults.exclude, 'e2e/*'],
+            exclude: [
+                ...configDefaults.exclude,
+                'e2e/*',
+                'src/App.vue',
+                'src/main.js',
+                'src/*.config.js',
+            ],
             root: fileURLToPath(new URL('./', import.meta.url)),
         },
     }),
