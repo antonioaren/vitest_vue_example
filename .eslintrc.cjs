3bd4692701a82f1eed17b56b461737cafb426752
Pedro Arenas <antonioaren2@gmail.com>
2024-03-10 10:36:27 +0100

    chore: Eslint configuration.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,14 @@
-/* eslint-env node */
-require('@rushstack/eslint-patch/modern-module-resolution')
-
 module.exports = {
-    root: true,
-    globals: {
-        module: 'readonly',
+    parser: 'vue-eslint-parser',
+    parserOptions: {
+        parser: '@typescript-eslint/parser',
     },
     extends: [
-        'plugin:vue/vue3-essential',
         'eslint:recommended',
-        '@vue/eslint-config-prettier/skip-formatting',
+        'plugin:vue/vue3-recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
     ],
-    parserOptions: {
-        ecmaVersion: 'latest',
-    },
     rules: {
         'vue/multi-word-component-names': 'off',
     },
